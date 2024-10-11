f05a59459388879837f0bc09979c0635460079d1
Argusto Lima <augusto.vlima2@gmai.com>
2024-10-10 21:25:59 -0300

    Atualizacao do properties

--- src/test/java/codesauro/api/ApiApplicationTests.java
+++ src/test/java/codesauro/api/ApiApplicationTests.java
@@ -1,13 +1,8 @@
 package codesauro.api;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ApiApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
